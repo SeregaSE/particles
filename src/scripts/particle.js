8694c388015eedabd76982548bfe86c870fa6350
Sergeev Sergey <sse1595@gmail.com>
2018-02-19 22:29:30 +0300

    transform: translate

--- src/scripts/particle.js
+++ src/scripts/particle.js
@@ -1,33 +1,28 @@
-import { animate, distanceBetweeen } from './utils';
+import { distanceBetween, getDocumentElementSize, getRandomNumber } from './utils';
 
 export default class Particle {
     constructor(initialX, initialY) {
         this.el = document.createElement('div');
-        this.el.style.position = 'absolute';
-        this.el.style.transition = 'all 0.1s ease 0s';
-        this.el.style['border-radius'] = '50%';
+        this.el.classList.add('particle');
 
+
+        // const { x, y } = this.getRandomPosition();
+        this.el.style.transform = `translate(${initialX}px, ${initialY}px)`;
         document.body.appendChild(this.el);
+        // window.addEventListener('resize', this.onWindowResize.bind(this));
 
-        this.setPosition(initialX, initialY);
-        this.setRandomSize();
-        this.setRandomColor();
-        this.move();
+        this.setSpeed();
+        this.moveRandomly();
     }
 
-    getPositionX() {
-        return this.getPixels('left');
+    onWindowResize() {
+        this.setSpeed();
+        this.moveRandomly();
     }
 
-    getPositionY() {
-        return this.getPixels('top');
-    }
-
-    getDocumentSize() {
-        return {
-            width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight,
-        };
+    getPosition() {
+        const { left:x, top:y } = this.el.getBoundingClientRect();
+        return { x, y }
     }
 
     getElementSize() {
@@ -46,93 +41,49 @@ export default class Particle {
         this.el.style[property] = [value, 'px'].join('');
     }
 
-    getRandomNumber(from, to) {
-        const rnd = Math.random();
-        return parseInt((rnd * (to - from)) + from);
-    }
-
-    setRandomColor() {
-        const rnd = Math.random();
-        const hex = 0x1000000 + rnd * 0xffffff;
-        const color = hex.toString(16).substr(1, 6);
-        this.el.style.background = ['#', color].join('');
-    }
-
     setPosition(x, y) {
         this.setPixels('left', x);
         this.setPixels('top', y);
     }
 
-    setRandomSize() {
-        const side = this.getRandomNumber(20, 70);
-        this.setPixels('width', side);
-        this.setPixels('height', side);
+    setSpeed() {
+        const { width, height } = getDocumentElementSize();
+        this.speed = (1 / Math.pow(width, 2) + 1 / Math.pow(height, 2)) * 10000000;
     }
 
-    calcNextCoords() {
-        const { width: toX, height: toY } = this.getDocumentSize();
-        const fromX = this.getPositionX() * -1;
-        const fromY = this.getPositionY() * -1;
-        const x = this.getRandomNumber(fromX, toX);
-        const y = this.getRandomNumber(fromY, toY);
-        return { x, y };
-    }
-
-    withRebound({ x, y }) {
+    getRandomPosition() {
         const { width: elWidth, height: elHeight } = this.getElementSize();
-        const { width, height } = this.getDocumentSize();
-        const maxWidth = width - elWidth;
-        const maxHeight = height - elHeight;
-        let nextX = x;
-        let nextY = y;
-
-        if (x <= 0) {
-            nextX = 0;
-        }
-
-        if (x >= maxWidth) {
-            nextX = maxWidth;
-        }
-
-        if (y <= 0) {
-            nextY = 0;
-        }
-
-        if (y >= maxHeight) {
-            nextY = maxHeight;
-        }
-
+        const { width, height } = getDocumentElementSize();
+        const toX = width - elWidth;
+        const toY = height - elHeight;
+        const x = getRandomNumber(0, toX);
+        const y = getRandomNumber(0, toY);
         return {
-            x: nextX,
-            y: nextY,
+            x,
+            y,
         }
     }
 
-    move() {
-        // calcNextCoords - Получаем случайную точку к которой будем двигаться
-        // withRebound - Если точка лежит за границей окна, сдвигаем точку до границы
-        const { x:targetX, y:targetY } = this.withRebound(this.calcNextCoords());
-        const curX = this.getPositionX();
-        const curY = this.getPositionY();
-        // Получаем расстояние между верхним левым краем точки и целевой точки
-        const distance = distanceBetweeen(
-            { x: targetX, y: targetY },
-            { x: curX, y: curY },
+    getDuration(target) {
+        const distance = distanceBetween(
+            target, // target point
+            this.getPosition(), // current point
         );
 
-        animate({
-            duration: distance  / 200 * 1000, // Задаем скорость
-            timing: progress => progress,
-            draw: progress => {
-                // Двигаем точку в зависимости от степени завершенности анимации
-                const nextX = curX + (targetX - curX) * progress;
-                const nextY = curY + (targetY - curY) * progress;
-                this.setPosition(nextX, nextY);
-                // Если закончили движение, начинаем новое
-                if (progress === 1) {
-                    this.move();
-                }
-            },
-        });
+        return distance / this.speed; // seconds
+    }
+
+    moveAt(target, duration) {
+        const { x, y } = target;
+        this.el.style.transition = `transform ${duration}s`;
+        this.el.style.transform = `translate(${x}px, ${y}px)`;
+    }
+
+    moveRandomly() {
+        if (this.timer) clearTimeout(this.timer);
+        const target = this.getRandomPosition();
+        const duration = this.getDuration(target);
+        this.moveAt(target, duration);
+        this.timer = setTimeout(this.moveRandomly.bind(this), duration * 1000);
     }
 }
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,23 +1,8 @@
-export const animate = ({ draw, duration, timing }) => {
-    const start = performance.now();
+export const distanceBetween = (a, b) => Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
 
-    const frame = time => {
-        let fraction = (time - start) / duration;
+export const getDocumentElementSize = () =>({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+});
 
-        if (fraction > 1) {
-            fraction = 1;
-        }
-
-        const progress = timing(fraction);
-
-        draw(progress);
-
-        if (fraction < 1) {
-            requestAnimationFrame(frame);
-        }
-    };
-
-    requestAnimationFrame(frame);
-};
-
-export const distanceBetweeen = (a, b) => Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+export const getRandomNumber = (from, to) => parseInt((Math.random() * (to - from)) + from);
